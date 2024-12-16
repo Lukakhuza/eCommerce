30073e70247eaf3eca71eeb9a2bee4bd2697a620
lukakhuza <lukakhuza7@gmail.com>
2024-12-16 05:30:59 -0500

    fetching code added

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,5 +1,38 @@
+import { useState, useEffect } from "react";
+
 function ProductsPage() {
-  return <h1> The Products Page</h1>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [fetchedProducts, setFetchedProducts] = useState();
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    async function fetchProducts() {
+      setIsLoading(true);
+      const response = await fetch("https://dummyjson.com/products");
+      if (!response.ok) {
+        setError("Fetching Products Failed.");
+      } else {
+        const resData = await response.json();
+        setFetchedProducts(resData.products);
+      }
+      setIsLoading(false);
+    }
+
+    fetchProducts();
+    console.log(fetchedProducts);
+  }, []);
+
+  // console.log();
+
+  return (
+    <>
+      <div>
+        {isLoading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+      </div>
+      {!isLoading && fetchedProducts && <p>We have products</p>}
+    </>
+  );
 }
 
 export default ProductsPage;
